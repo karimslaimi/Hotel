1c943401a8bf11e664e7d6d16e13114f921a2d64
mohamed karim sleimi <karim-nar@live.fr>
2019-09-06 16:21:30 +0100

    initial commit wish it won't take so long

---
+++ Hotel/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Hotel.Models
+{
+    public class User
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string mail { get; set; }
+        public string password { get; set; }
+        public string type { get; set; }
+
+    }
+}
---
+++ Hotel/Models/depenses.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hotel.Models
+{
+    public class depenses
+    {
+        public int id { get; set; }
+        public string motif { get; set; }
+    }
+}
