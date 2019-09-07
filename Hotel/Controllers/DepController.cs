d166be770cbfe371091df094876f77a7844d1b5e
mohamed karim sleimi <karim-nar@live.fr>
2019-09-07 16:25:53 +0100

    creating controllers

---
+++ Hotel/Controllers/DepController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hotel.Controllers
+{
+    public class DepController : Controller
+    {
+        // GET: Dep
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
